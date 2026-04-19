2a769ce6ee083ed3ec43206bebdedcbd1d00c555
박준현 <er65119@naver.com>
2026-04-19 18:26:52 +0900

    [KAN-46] 카카오 SDK 초기화 및 런치 크래시 방지

--- Pickflow/Sources/App/PickflowApp.swift
+++ Pickflow/Sources/App/PickflowApp.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import KakaoSDKCommon
 
 @main
 struct PickflowApp: App {
@@ -6,11 +7,26 @@ struct PickflowApp: App {
 
     init() {
         DesignSystemFontRegister.registerAllCustomFonts()
+        initializeKakaoSDK()
     }
 
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+
+    private func initializeKakaoSDK() {
+        guard let appKey = Bundle.main.object(forInfoDictionaryKey: "KAKAO_NATIVE_APP_KEY") as? String,
+              appKey.isEmpty == false,
+              appKey != "YOUR_KAKAO_NATIVE_APP_KEY"
+        else {
+            #if DEBUG
+            print("Skipping KakaoSDK initialization: KAKAO_NATIVE_APP_KEY is missing or still using a placeholder value.")
+            #endif
+            return
+        }
+
+        KakaoSDK.initSDK(appKey: appKey)
     }
 }
